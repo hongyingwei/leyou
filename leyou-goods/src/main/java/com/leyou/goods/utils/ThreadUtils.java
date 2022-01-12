09a4dcaa7300d577a770e109aea0f8de014d697c
御风 <54386576+hongyingwei@users.noreply.github.com>
2022-01-12 19:54:59 +0800

    前台的全文检索修改-2

--- leyou-goods/src/main/java/com/leyou/goods/utils/ThreadUtils.java
+++ leyou-goods/src/main/java/com/leyou/goods/utils/ThreadUtils.java
@@ -3,6 +3,9 @@ package com.leyou.goods.utils;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * 全文检索的线程池设置
+ */
 public class ThreadUtils {
     private static final ExecutorService es = Executors.newFixedThreadPool(10);
 
