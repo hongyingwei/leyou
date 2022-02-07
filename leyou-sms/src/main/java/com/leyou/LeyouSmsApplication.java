f49538c5b021c71a3568cd5d6f77a90e4318b1fd
御风 <54386576+hongyingwei@users.noreply.github.com>
2022-02-07 21:27:20 +0800

    短信微服务模块-创建短信微服务模块

--- leyou-sms/src/main/java/com/leyou/LeyouSmsApplication.java
+++ leyou-sms/src/main/java/com/leyou/LeyouSmsApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @author
- * @description
+ * @author hyw
+ * @description 短信微服务
  */
 @SpringBootApplication
 public class LeyouSmsApplication {
