169c7afb2a3ad6122562695a365ff929b9f6fa2f
御风 <54386576+hongyingwei@users.noreply.github.com>
2022-02-08 21:12:07 +0800

    User微服务模块-redis验证码缓存，发送消息队列

--- leyou-user/leyou-user-service/src/main/java/com/leyou/user/service/UserService.java
+++ leyou-user/leyou-user-service/src/main/java/com/leyou/user/service/UserService.java
@@ -1,9 +1,18 @@
 package com.leyou.user.service;
 
+import com.leyou.common.utils.NumberUtils;
 import com.leyou.user.mapper.UserMapper;
 import com.leyou.user.pojo.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author hyw
@@ -13,6 +22,18 @@ import org.springframework.stereotype.Service;
 public class UserService {
     @Autowired
     private UserMapper userMapper;
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @Autowired
+    private AmqpTemplate amqpTemplate;
+
+    //短信模版
+    static final String KEY_PREFIX = "user:code:phone:";
+
+    //打印日志
+    static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
     /**
      * 效验用户或手机号是否存在
@@ -33,5 +54,30 @@ public class UserService {
                 return null;
         }
         return this.userMapper.selectCount(user) == 0; //检测到有数据，说明已存在该用户，检测到没有数据，说明不存在该用户
+    }
+
+    /**
+     * 生成短信验证码
+     * @param phone
+     * @return
+     */
+    public boolean sendVerifyCode(String phone){
+        //1、生成验证码
+        String code = NumberUtils.generateCode(6);
+
+        try {
+            //2、发送短信
+            Map<String, String> msg = new HashMap<>();
+            msg.put("phone", phone);
+            msg.put("code", code);
+            this.amqpTemplate.convertAndSend("leyou.sms.exchange", "sms.verify.code", msg);
+
+            //3、验证码缓存到redis，便于注册验证
+            this.redisTemplate.opsForValue().set(KEY_PREFIX + phone, code, 5, TimeUnit.HOURS);
+            return true;
+        } catch (Exception e) {
+            logger.error("发送短信失败。phone：{}， code：{}", phone, code);
+            return false;
+        }
     }
 }
