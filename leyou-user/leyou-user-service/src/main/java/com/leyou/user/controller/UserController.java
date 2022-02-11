ae131b4293cd3fb7bd09b1fe779a32e2f4ebd79b
御风 <54386576+hongyingwei@users.noreply.github.com>
2022-02-11 22:40:01 +0800

    User微服务模块-无状态登录

--- leyou-user/leyou-user-service/src/main/java/com/leyou/user/controller/UserController.java
+++ leyou-user/leyou-user-service/src/main/java/com/leyou/user/controller/UserController.java
@@ -73,7 +73,7 @@ public class UserController {
     public ResponseEntity<User> queryUser(
             @RequestParam("username") String username,
             @RequestParam("password") String password
-    ) {
+    ){
         User user = this.userService.queryUser(username, password);
         if (user == null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
