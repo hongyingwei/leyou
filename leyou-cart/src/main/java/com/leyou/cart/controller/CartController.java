f130ab039a2e1a542342d107027755c090c48b55
御风 <54386576+hongyingwei@users.noreply.github.com>
2022-02-18 21:49:00 +0800

    购物车微服务模块-添加购物车

---
+++ leyou-cart/src/main/java/com/leyou/cart/controller/CartController.java
@@ -0,0 +1,27 @@
+package com.leyou.cart.controller;
+
+import com.leyou.cart.pojo.Cart;
+import com.leyou.cart.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
+public class CartController {
+
+    @Autowired
+    private CartService cartService;
+
+    /**
+     * 添加购物车
+     *
+     * @return
+     */
+    @PostMapping
+    public ResponseEntity<Void> addCart(@RequestBody Cart cart) {
+        this.cartService.addCart(cart);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ leyou-cart/src/main/java/com/leyou/cart/service/CartService.java
@@ -0,0 +1,14 @@
+package com.leyou.cart.service;
+
+import com.leyou.cart.pojo.Cart;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author
+ * @description
+ */
+@Service
+public class CartService {
+    public void addCart(Cart cart) {
+    }
+}
